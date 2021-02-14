0a3ad53df65d5c86a4fccc624f712f5fcb70cbbc
Madhura Parikh <MadhuraParikh@twosigma.com>
2021-02-14 12:46:43 -0500

    add basic skeleton class

---
+++ src/GameOfLife.cpp
@@ -0,0 +1,29 @@
+#include "src/GameOfLife.h"
+
+GameOfLife::GameOfLife(uint64_t height, uint64_t width, uint64_t maxIters, bool useSIMD):
+    height_(height),
+    width_(width),
+    maxIters_(maxIters),
+    useSIMD_(useSIMD) {}
+
+void GameOfLife::run() {
+  if (useSIMD_) {
+    runSIMD();
+  } else {
+    runScalar();
+  }
+}
+
+void GameOfLife::randomInit() {
+  // impl
+}
+
+void GameOfLife::runScalar() {
+  // impl
+}
+
+void GameOfLife::runSIMD() {
+  // impl
+}
+
+
---
+++ src/GameOfLife.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <vector>;
+
+class GameOfLife {
+
+  public:
+    GameOfLife(uint64_t gridHeight, uint64_t gridWidth, uint64_t maxIters, bool useSIMD);
+    void randomInit();
+    void run();
+
+  private:
+    uint64_t height_;
+    uint64_t width_;
+    uint64_t maxIters_;
+    std::vector<std::vector<bool>> grid_;
+
+    bool useSIMD_;
+
+    runScalar();
+    runSIMD();
+};
+
+
